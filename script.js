8943df1b1834d321d940219f6e268c9772fdddd0
Rafael Cardoso <faelcardoso8@gmail.com>
2024-01-19 22:03:43 -0300

    19-01-24

--- script.js
+++ script.js
@@ -12,13 +12,48 @@
             var idade = ano - Number(entradaano.value)
             //res.innerHTML = (`Idade calculada: ${idade}`)
             var genero = ''
+            var img = document.createElement('img')
+            img.setAttribute('id' , 'foto')
             if(fsex[0].checked){
                 genero = 'Masculino'
+                if(idade < 12){
+                    //criança
+                    img.setAttribute('src', 'criança.png')
+                }
+                else if(idade < 18){
+                    //jovem
+                    img.setAttribute('src' , 'jovem.png')
+                }
+                else if(idade < 50){
+                    //adulto
+                    img.setAttribute('src' , 'adulto.png')
+                }
+                else{
+                    //idoso
+                    img.setAttribute('src' , 'idoso.png')
+                }
             }
             else{
                 genero = 'Feminino'
+                if(idade < 12){
+                    //criança
+                    img.setAttribute('src', 'criança-F.png')
+                }
+                else if(idade < 18){
+                    //jovem
+                    img.setAttribute('src' , 'jovem-F.png')
+                }
+                else if(idade < 50){
+                    //adulto
+                    img.setAttribute('src' , 'adulto-F.png')
+                }
+                else{
+                    //idoso
+                    img.setAttribute('src' , 'idoso-F.png')
+                }
             }
             res.innerHTML =(`Idade calculada é ${idade} e o genero é ${genero}.`)
+            res.appendChild(img)
             
         }
 
